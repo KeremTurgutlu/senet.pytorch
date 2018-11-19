91e471123c8d626be25a0db1f05a803a2b49c2f7
Kerem Turgutlu <keremturgutlu@gmail.com>
2018-11-18 19:29:23 -0800

    Update se_resnet.py
    fixed return for preact model

--- se_resnet.py
+++ se_resnet.py
@@ -235,7 +235,7 @@ class CifarSEPreActResNet(CifarSEResNet):
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
-
+        return x
 
 def se_resnet20(**kwargs):
     """Constructs a ResNet-18 model.
